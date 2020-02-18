b197e3faa38c511d41938c083a1e592565bd121d
dewabrata <dewabrata@gmail.com>
2020-02-18 14:32:10 +0700

    Initial commit

---
+++ app/src/main/java/com/juara/daftarfilm/Service/APIInterfacesRest.java
@@ -0,0 +1,47 @@
+package com.juara.daftarfilm.Service;
+
+public interface APIInterfacesRest {
+
+
+    /*
+     @FormUrlEncoded
+    @POST("api/komplain/add")
+    Call<UpdateKomplain> addKomplain(@Field("username_komplain") String username, @Field("kode_edc") String kode_edc, @Field("masalah") String masalah, @Field("notes_komplain") String notes);
+    @GET("api/edc_problem/all")
+    Call<MasalahEdcModel> getEDCProblem();
+*//*
+    @GET("api/dataorder/all")
+    Call<ModelOrder> getOrder(@Query("username") String user);
+/*
+            @Part MultipartBody.Part img1,
+           @Part MultipartBody.Part img2,
+           @Part MultipartBody.Part img3,
+ *//*
+   @Multipart
+   @POST("api/dataorder/update")
+   Call<Komplain> updateData(
+           @Part("pod_date") RequestBody podate,
+           @Part("status") RequestBody status,
+           @Part("lat") RequestBody lat,
+           @Part("lon") RequestBody lon,
+           @Part("poddate") RequestBody poddate,
+           @Part("recievedate") RequestBody recievedate,
+           @Part("id") RequestBody id,
+           @Part MultipartBody.Part img1
+   );
+   @Multipart
+   @POST("api/komplain/update")
+   Call<UpdateKomplain> sendImage(
+           @Part("id") RequestBody id,
+           @Part("username_penanganan") RequestBody username_komplain,
+           @Part("hasil_penanganan") RequestBody masalah,
+           @Part("tanggal_penanganan") RequestBody kode_edc,
+           @Part("notes_penanganan") RequestBody notes_komplain,
+           @Part("status") RequestBody status,
+           @Part MultipartBody.Part img1
+   );*/
+
+}
+
+
+
