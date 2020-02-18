6fff4dab5b5c8684b7e694eaf88a066800185af8
dewabrata <dewabrata@gmail.com>
2020-02-18 18:44:26 +0700

    Menampilkan Data search dee

--- app/src/main/java/com/juara/daftarfilm/MainActivity.java
+++ app/src/main/java/com/juara/daftarfilm/MainActivity.java
@@ -7,9 +7,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.juara.daftarfilm.Model.DaftarFilm.DaftarFilmModel;
@@ -37,7 +40,28 @@ EditText txtSearch;
         setContentView(R.layout.activity_main);
         rc = findViewById(R.id.rc);
         btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setVisibility(View.INVISIBLE);
         txtSearch = findViewById(R.id.txtSearch);
+   /*     txtSearch.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Toast.makeText(MainActivity.this,txtSearch.getText().toString(),Toast.LENGTH_SHORT).show();
+             //
+                //   callDaftarFilm(txtSearch.getText().toString());
+                return false;
+            }
+        });*/
+
+   txtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+       @Override
+       public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+          if (actionId == 5){
+              callDaftarFilm(txtSearch.getText().toString());
+          }
+           return false;
+       }
+   });
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
